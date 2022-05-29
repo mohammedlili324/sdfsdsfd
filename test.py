07e3014b27651e2ecf6afdaf9ec7de676fbbf171
moidata <77037486+mohammedlili324@users.noreply.github.com>
2022-05-29 05:28:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,31 +13,29 @@ import pandas as pd
 app = Flask(__name__)
 
 
+def s():
+        window = tk.Tk()
 
+        file = askopenfile(filetypes=[('csv Files', '*.csv')])
+        pdf_file = open(file.name, 'rb')
 
+        s = pd.read_csv(pdf_file)
+        h = open('dsf.html', 'w')
+        e = s.to_html(h)
+        h.close()
+        put_html(e)
 
+        se = open('dsf.html', 'rb').read()
+        put_file('dsf.html', se, 'dsf.html')
+        window.destroy()
 def start():
-        def s():
-                window = tk.Tk()
 
-                file = askopenfile(filetypes=[('csv Files', '*.csv')])
-                pdf_file = open(file.name, 'rb')
-
-                s = pd.read_csv(pdf_file)
-                h = open('dsf.html', 'w')
-                e = s.to_html(h)
-                h.close()
-                put_html(e)
-
-                se = open('dsf.html', 'rb').read()
-                put_file('dsf.html', se, 'dsf.html')
-                window.destroy()
 
         put_button(label='dsf', onclick=s)
 
 
 
-app.add_url_rule('/tool','webio.view',webio_view(start),methods=['GET','POST','OPTIONS'])
+app.add_url_rule('/tool','webio.view',webio_view(start,s),methods=['GET','POST','OPTIONS'])
 if __name__== "__main__":
         parser = argparse.ArgumentParser()
         parser.add_argument("-p" , "--port",type=int,default=8080)

fc235b4a79d14c1dcde1bfb1372fcbd1f9c84043
moidata <77037486+mohammedlili324@users.noreply.github.com>
2022-07-06 04:11:53 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,38 @@
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.pin import *
+from pywebio.platform import *
+import argparse
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import download
+from pywebio import start_server
+from pywebio.platform.flask import webio_view
+from pywebio.platform import *
+from pywebio import STATIC_PATH
+from flask import Flask,send_from_directory
+import cv2
+
+app = Flask(__name__)
+def tk():
+    a = cv2.VideoCapture(0)
+    ret, im = a.read()
+    cv2.imwrite('asdasd.png', im)
+
+    img = open(r'C:\Users\moi data siencets\Desktop\moi21\face_and_eye_detection_app\asdasd.png', 'rb').read()
+    popup('',put_image(img))
+
+def start():
+
+
+
+    put_grid([[None,None,put_button(label='take pic',onclick=tk),None,None]])
+
+app.add_url_rule('/tool','webio.view',webio_view(start)
+                 ,methods=['GET','POST','OPTIONS'])
+
+if __name__== "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-p", "--port",type=int,default=8080)
+        args = parser.parse_args()
+        start_server(start,port=args.port,debug=True)

ef10b1d7dd81d9dc072777f87950ffb0cd2a4588
moidata <77037486+mohammedlili324@users.noreply.github.com>
2022-07-06 04:25:04 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,28 +4,26 @@ from pywebio.output import *
 from pywebio.session import download
 from pywebio import start_server
 from pywebio.platform.flask import webio_view
+from pywebio.platform import *
 from pywebio import STATIC_PATH
 from flask import Flask,send_from_directory
 import cv2
 
 app = Flask(__name__)
-def tk():
-    a = cv2.VideoCapture(0)
-    ret, im = a.read()
-    cv2.imwrite('asdasd.png', im)
-
-    img = open(r'asdasd.png', 'rb').read()
-    popup('',put_image(img))
 
 def start():
+    def tk():
+        a = cv2.VideoCapture(0)
+        ret, im = a.read()
+        cv2.imwrite('asdasd.png', im)
 
-
+        img = open(r'asdasd.png', 'rb').read()
+        popup('', put_image(img))
 
     put_grid([[None,None,put_button(label='take pic',onclick=tk),None,None]])
 
 app.add_url_rule('/tool','webio.view',webio_view(start)
                  ,methods=['GET','POST','OPTIONS'])
-
 
 if __name__== "__main__":
         parser = argparse.ArgumentParser()
